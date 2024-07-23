f65115973e317a5d4bc3a30963962e281af7913d
linyuye <54428773+linyuye@users.noreply.github.com>
2024-07-23 18:27:28 +0800

    Add files via upload

---
+++ b站使用wbi签名的爬取方式.py
@@ -0,0 +1,104 @@
+from functools import reduce
+from hashlib import md5
+import urllib.parse
+import time
+import json
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+mixinKeyEncTab = [
+    46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
+    33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
+    61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
+    36, 20, 34, 44, 52
+]
+
+def getMixinKey(orig: str):
+    '对 imgKey 和 subKey 进行字符顺序打乱编码'
+    return reduce(lambda s, i: s + orig[i], mixinKeyEncTab, '')[:32]
+
+def encWbi(params: dict, img_key: str, sub_key: str):
+    '为请求参数进行 wbi 签名'
+    mixin_key = getMixinKey(img_key + sub_key)
+    curr_time = round(time.time())
+    params['wts'] = curr_time                                   # 添加 wts 字段
+    params = dict(sorted(params.items()))                       # 按照 key 重排参数
+    # 过滤 value 中的 "!'()*" 字符
+    params = {
+        k : ''.join(filter(lambda chr: chr not in "!'()*", str(v)))
+        for k, v 
+        in params.items()
+    }
+    query = urllib.parse.urlencode(params)                      # 序列化参数
+    wbi_sign = md5((query + mixin_key).encode()).hexdigest()    # 计算 w_rid
+    params['w_rid'] = wbi_sign
+    return params
+
+def getWbiKeys() -> tuple[str, str]:
+    '获取最新的 img_key 和 sub_key'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Referer': 'https://www.bilibili.com/'
+    }
+    resp = requests.get('https://api.bilibili.com/x/web-interface/nav', headers=headers)
+    resp.raise_for_status()
+    json_content = resp.json()
+    img_url: str = json_content['data']['wbi_img']['img_url']
+    sub_url: str = json_content['data']['wbi_img']['sub_url']
+    img_key = img_url.rsplit('/', 1)[1].split('.')[0]
+    sub_key = sub_url.rsplit('/', 1)[1].split('.')[0]
+    return img_key, sub_key
+
+img_key, sub_key = getWbiKeys()
+"""
+需要修改这里的oid和下面的oid，二者都需要修改
+"""
+signed_params = encWbi(
+    params={
+        'oid': 1504733402,
+        'type': 1,
+        'mode': 3,
+        'plat': 1,
+        'next': 0,
+    },
+    img_key=img_key,
+    sub_key=sub_key
+)
+query = urllib.parse.urlencode(signed_params)
+print(signed_params)
+print(query)
+parsed_query = urllib.parse.parse_qs(query)
+wts = parsed_query.get('wts', [None])[0]
+w_rid = parsed_query.get('w_rid', [None])[0]
+
+# 请求头
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Referer': 'https://www.bilibili.com/',
+}
+
+# 初始化Session和重试策略
+session = requests.Session()
+retries = Retry(total=3, backoff_factor=0.1, status_forcelist=[500, 502, 503, 504])
+session.mount('https://', HTTPAdapter(max_retries=retries))
+
+# 新的数据
+data = {
+    'oid': 1504733402,
+    'type': 1,
+    'mode': 3,
+    'plat': 1,
+    'next': 0,
+    'w_rid': w_rid,
+    'wts': wts
+}
+# 发送请求
+url = 'https://api.bilibili.com/x/v2/reply/wbi/main?'
+response = requests.get(url, params=data,headers=headers)
+print(response.url)  # 打印请求的URL
+print(response.json())  # 打印响应的JSON内容
+
+
+
+
+
